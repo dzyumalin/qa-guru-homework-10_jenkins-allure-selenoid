27c44bc764072c0f01c0cbe3a552af95a3505902
dmitryhli <zyumalin@bk.ru>
2021-06-24 05:27:37 +0300

    recovery properties

--- src/test/java/tests/OwnerTests.java
+++ src/test/java/tests/OwnerTests.java
@@ -1,33 +1,33 @@
-//package tests;
-//
-//import static config.Credential.credentials;
-//
-//import org.junit.jupiter.api.Tag;
-//import org.junit.jupiter.api.Test;
-//
-//public class OwnerTests {
-//
-//    @Test
-//    @Tag("owner")
-//    void readFromPropertiesTest() {
-//        String login = credentials.login();
-//        String password = credentials.password();
-//
-//        System.out.println(login);
-//        System.out.println(password);
-//
-//
-//        String message = String.format("I try to login as %s with password %s", login, password);
-//
-//        System.out.println(message);
-//        // gradle clean owner_test
-//        // qaguru
-//        // secret$password
-//        // I try to login as github with password secret$password
-//
-//        // gradle clean owner_test -Dlogin=github
-//        // github
-//        // secret$password
-//        // I try to login as github with password secret$password
-//    }
-//}
+package tests;
+
+import static config.Credential.credentials;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class OwnerTests {
+
+    @Test
+    @Tag("owner")
+    void readFromPropertiesTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+
+        String message = String.format("I try to login as %s with password %s", login, password);
+
+        System.out.println(message);
+        // gradle clean owner_test
+        // qaguru
+        // secret$password
+        // I try to login as github with password secret$password
+
+        // gradle clean owner_test -Dlogin=github
+        // github
+        // secret$password
+        // I try to login as github with password secret$password
+    }
+}
